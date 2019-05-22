c31286727372458708ed916de2247f9af33a8a08
Diogo Martino <diogo.martino10@gmail.com>
2019-05-22 18:38:15 +0100

    test

---
+++ ThrottleStopStarter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThrottleStopStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(2000);
+            Process.Start(@"C:\Users\Martino\Documents\ThrottleStop\ThrottleStop.exe");
+            Environment.Exit(-1);
+        }
+    }
+}
